5cb6bbefd6ad58f86e5154b164e28cd4d4de6f10
Ryan Nixon <r.taiidani@gmail.com>
2024-11-17 19:23:07 -0800

    Adding multiple tabs to the game view

--- internal/server/assets/index.js
+++ internal/server/assets/index.js
@@ -1,1 +1,29 @@
-// No JS at this point!
+let app = document.getElementById("app");
+if (app) {
+    let nav = document.querySelector("#app > nav");
+
+    nav.addEventListener("click", function (evt) {
+        evt.stopPropagation();
+
+        let btns = nav.querySelectorAll("button");
+        btns.forEach(element => {
+            if (element === evt.target) {
+                element.className = "";
+                element.ariaCurrent = "true";
+            } else {
+                element.className = "outline";
+                element.ariaCurrent = "false";
+            }
+        });
+
+        let t = evt.target.dataset.target;
+        let tabs = app.querySelectorAll(".tab");
+        tabs.forEach(element => {
+            if (element.id === t) {
+                element.classList.remove("hide");
+            } else {
+                element.classList.add("hide");
+            }
+        });
+    });
+}
